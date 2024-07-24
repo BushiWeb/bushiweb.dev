88df30ad9bbbcc3cc5a95fc7d20432e72f505bb0
Emeric <emeric.francisod+github@gmail.com>
2024-07-24 09:09:52 +0200

    🐛fix(footer): fix footer nav position

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -8,7 +8,7 @@ import { FooterNav } from './FooterNav';
  */
 export const Footer = () => {
     return (
-        <footer className="flex flex-col flex-nowrap items-stretch justify-center bg-primary-background px-300 py-150 text-primary-on-container">
+        <footer className="flex flex-col flex-nowrap items-stretch justify-center bg-primary-background text-primary-on-container">
             <div className="flex flex-col flex-nowrap items-center justify-start gap-200 px-300 py-150 desktop:flex-row desktop:flex-wrap desktop:justify-between desktop:px-500 desktop:py-250">
                 <Link href="/">
                     <Logo className="h-500 w-auto" />
