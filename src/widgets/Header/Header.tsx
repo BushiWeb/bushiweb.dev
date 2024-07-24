33f8d65efa871bc891882f9477cbc4b7073df5b6
Emeric <emeric.francisod+github@gmail.com>
2024-07-24 09:17:33 +0200

    🐛fix(header): fix icon sizing

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 export const Header = () => {
     return (
         <header className="flex h-1000 flex-row flex-nowrap items-center justify-center bg-primary-background px-300 py-200 text-primary-on-container">
-            <Link href="/">
+            <Link href="/" className="h-full">
                 <Logo className="max-h-full w-auto" />
             </Link>
         </header>
