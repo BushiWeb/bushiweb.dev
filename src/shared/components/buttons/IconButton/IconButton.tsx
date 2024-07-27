3055c0d3535e41fa121115654d1b2192e952d537
Emeric <emeric.francisod+github@gmail.com>
2024-07-27 17:19:24 +0200

    🐛fix(iconbutton): fix clickable area
    The pseudo element that was meant to increase the clickable area of the
    button was not doing its job. Fixed it.

--- src/shared/components/buttons/IconButton/IconButton.tsx
+++ src/shared/components/buttons/IconButton/IconButton.tsx
@@ -28,14 +28,15 @@ export const IconButton = ({
             onPress={onPress}
             aria-label={label}
             className={clsx(
-                'relative',
+                'relative isolate',
                 'grid place-items-center',
                 'h-500 w-500',
                 'bg-transparent text-secondary',
                 'rounded-full',
                 'text-l2',
+                'm-100',
                 'before:absolute before:z-high before:h-500 before:w-500 before:rounded-full before:bg-secondary before:opacity-0',
-                'after:absolute after:z-low after:h-600 after:w-600 after:bg-transparent',
+                'after:absolute after:-inset-100 after:z-low after:bg-transparent',
                 'before:hover:opacity-10',
                 'before:focus:opacity-15',
                 'before:pressed:opacity-15',
