b511a4d8db540fb1fdf5442ec6c5c65e4178fbfb
Emeric <emeric.francisod+github@gmail.com>
2024-07-29 09:58:23 +0200

    ♻️ refactor(buttons): use React Aria states syntax
    Use functions in className to increase readability of tailwind classes.

--- src/shared/components/buttons/Button/Button.tsx
+++ src/shared/components/buttons/Button/Button.tsx
@@ -34,32 +34,34 @@ export const Button = ({
         <AriaButton
             type={type}
             onPress={onPress}
-            className={clsx(
-                'h-500 min-w-500',
-                'px-250 py-150',
-                'my-100',
-                'flex flex-row items-center gap-100',
-                'font-primary text font-semibold leading-tight',
-                'rounded-500',
-                'relative isolate',
-                'before:absolute before:inset-0 before:z-high before:rounded-500 before:opacity-0',
-                'after:absolute after:-bottom-100 after:-top-100 after:left-0 after:right-0 after:z-low after:min-w-600 after:bg-transparent',
-                'before:hover:opacity-10',
-                'before:focus:opacity-15',
-                'before:pressed:opacity-15',
-                'focus-visible:focus-ring',
-                variant === 'primary' && [
-                    'bg-primary text-primary-on before:bg-primary-on',
-                ],
-                variant === 'secondary' && [
-                    'bg-secondary-container text-secondary-on-container before:bg-secondary-on-container',
-                ],
-                variant === 'tertiary' && [
-                    'border border-outline text-primary before:bg-primary',
-                ],
-                variant === 'text' && ['text-primary before:bg-primary'],
-                className,
-            )}
+            className={({ isHovered, isFocusVisible, isFocused, isPressed }) =>
+                clsx(
+                    'h-500 min-w-500',
+                    'px-250 py-150',
+                    'my-100',
+                    'flex flex-row items-center gap-100',
+                    'font-primary text font-semibold leading-tight',
+                    'rounded-500',
+                    'relative isolate',
+                    'before:absolute before:inset-0 before:z-high before:rounded-500 before:opacity-0',
+                    'after:absolute after:-bottom-100 after:-top-100 after:left-0 after:right-0 after:z-low after:min-w-600 after:bg-transparent',
+                    isHovered && 'before:opacity-10',
+                    isFocused && 'before:opacity-15',
+                    isPressed && 'before:opacity-15',
+                    isFocusVisible && 'focus-ring',
+                    variant === 'primary' && [
+                        'bg-primary text-primary-on before:bg-primary-on',
+                    ],
+                    variant === 'secondary' && [
+                        'bg-secondary-container text-secondary-on-container before:bg-secondary-on-container',
+                    ],
+                    variant === 'tertiary' && [
+                        'border border-outline text-primary before:bg-primary',
+                    ],
+                    variant === 'text' && ['text-primary before:bg-primary'],
+                    className,
+                )
+            }
         >
             {icon && <FontAwesomeIcon icon={icon} />}
             {children}
--- src/shared/components/buttons/IconButton/IconButton.tsx
+++ src/shared/components/buttons/IconButton/IconButton.tsx
@@ -27,22 +27,24 @@ export const IconButton = ({
         <Button
             onPress={onPress}
             aria-label={label}
-            className={clsx(
-                'relative isolate',
-                'grid place-items-center',
-                'h-500 w-500',
-                'bg-transparent text-secondary',
-                'rounded-full',
-                'text-l2',
-                'm-100',
-                'before:absolute before:z-high before:h-500 before:w-500 before:rounded-full before:bg-secondary before:opacity-0',
-                'after:absolute after:-inset-100 after:z-low after:bg-transparent',
-                'before:hover:opacity-10',
-                'before:focus:opacity-15',
-                'before:pressed:opacity-15',
-                'focus-visible:focus-ring',
-                className,
-            )}
+            className={({ isHovered, isFocusVisible, isFocused, isPressed }) =>
+                clsx(
+                    'relative isolate',
+                    'grid place-items-center',
+                    'h-500 w-500',
+                    'bg-transparent text-secondary',
+                    'rounded-full',
+                    'text-l2',
+                    'm-100',
+                    'before:absolute before:z-high before:h-500 before:w-500 before:rounded-full before:bg-secondary before:opacity-0',
+                    'after:absolute after:-inset-100 after:z-low after:bg-transparent',
+                    isHovered && 'before:opacity-10',
+                    isFocused && 'before:opacity-15',
+                    isPressed && 'before:opacity-15',
+                    isFocusVisible && 'focus-ring',
+                    className,
+                )
+            }
         >
             <FontAwesomeIcon icon={icon} />
         </Button>
